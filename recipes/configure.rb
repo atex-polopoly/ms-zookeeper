ba5ca3daac2dbc246bd64da0e9badee1c10d8c75
morblom <morblom@atex.com>
2018-02-26 13:56:58 +0100

    Adds user, group and sets up ownership of the installation. Started with the configuration

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+
+
+file '/srv/zookeeper/data/myid' do
+  content '1'#server id, needs some fancy schmancy logic to determine
+end
+
+template 'zoo.cnf' do
+  source 'zoo.cnf.erb'
+end
--- recipes/install.rb
+++ recipes/install.rb
@@ -6,17 +6,34 @@
 
 include_recipe 'jdk::install'
 
+user 'zookeeper'
+
+group zookeeper do
+  member 'zookeeper'
+end
+
 #Should validate checksum
-remote_file "#{Chef::Config[:file_cache_path]}/zookeeper-#{node['zookeeper']['version']}.tar.gz" do
+remote_file "#{Chef::Config[:file_cache_path]}/zookeeper.tar.gz" do
   source "ftp://10.10.10.10/mirror/zookeeper/zookeeper-#{node['zookeeper']['version']}.tar.gz"
   ftp_active_mode node['ftp_active_mode']
   not_if { 	File.exists?('/srv/zookeeper') }
 end
 
 
-execute "untar-zookeeper" do
+execute "install zookeeper" do
   cwd Chef::Config[:file_cache_path]
-  command "tar -xf #{Chef::Config[:file_cache_path]}/zookeeper-#{node['zookeeper']['version']}.tar.gz"
+  command "tar -xf #{Chef::Config[:file_cache_path]}/zookeeper.tar.gz -C /srv/zookeeper"
   not_if { File.exists?('/srv/zookeeper') }
-  only_if { File.exists?("#{Chef::Config[:file_cache_path]}/zookeeper-#{node['zookeeper']['version']}.tar.gz") }
+  only_if { File.exists?("#{Chef::Config[:file_cache_path]}/zookeeper.tar.gz") }
+end
+
+execute 'chown zookeeper' do
+  execute 'chown zookeeper:zookeeper -R /srv/zookeeper'
+  not_if { File.stat('/srv/zookeeper').uid == x%(id -u zookeeper) &&  File.stat('/srv/zookeeper').gid == x%(id -g zookeeper) }
+end
+
+directory '/srv/zookeeper/conf' do
+  user 'zookeeper'
+  group 'zookeeper'
+  only_if { File.exists?('/srv/zookeeper') }
 end
