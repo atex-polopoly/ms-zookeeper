150d833c637b949922406e6dbbe99d75b40c2997
morblom <morblom@atex.com>
2018-03-14 22:36:58 +0100

    Fixed last test errors (errors were in tests)

--- test/smoke/default/configure.rb
+++ test/smoke/default/configure.rb
@@ -14,15 +14,15 @@ describe file('/srv/zookeeper/data/myid') do
   its('content') { should include('3')}
 end
 
-describe file('/srv/zookeeper/conf/zoo.cnf') do
+describe file('/srv/zookeeper/conf/zoo.cfg') do
   it { should exist }
   it { should be_file }
   it { should be_owned_by 'zookeeper' }
   it { should be_grouped_into 'zookeeper' }
   its('mode') { should cmp '00644' }
-  its('content') { should include('server.1=zookeeper.1.prod.atx.cloud.atex.com:2888:3888')}
-  its('content') { should include('server.2=zookeeper.2.prod.atx.cloud.atex.com:2888:3888')}
-  its('content') { should include('server.3=zookeeper.3.prod.atx.cloud.atex.com:2888:3888')}
+  its('content') { should include('server.1=zoo1.prod.atx.cloud.atex.com:2888:3888')}
+  its('content') { should include('server.2=zoo2.prod.atx.cloud.atex.com:2888:3888')}
+  its('content') { should include('server.3=zoo3.prod.atx.cloud.atex.com:2888:3888')}
   its('content') { should include('dataDir=/srv/zookeeper/data')}
 end
 
@@ -32,5 +32,5 @@ describe file('/srv/zookeeper/data') do
   it { should be_grouped_into 'zookeeper' }
   its('link_path') { should eq '/mnt/data/zookeeper/data' }
   its('mode') { should cmp '00755' }
-  its('type') { should cmp 'link' }
+  its('type') { should cmp 'directory' }
 end
